d27fcb6ddccd38610887c7a94e06525918167f6d
Eden Wang <edwardwang33773@gmail.com>
2024-08-28 21:54:23 +0800

    update mybatis-spring-boot-starter to `3`

---
+++ src/main/java/org/ember/demo/service/StudentService.java
@@ -0,0 +1,14 @@
+package org.ember.demo.service;
+
+import com.mybatisflex.core.service.IService;
+import org.ember.demo.entity.Student;
+
+/**
+ *  服务层。
+ *
+ * @author Eden
+ * @since 2024-08-28
+ */
+public interface StudentService extends IService<Student> {
+
+}
